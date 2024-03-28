fdbe23e3fb549a53cc585aedf78549eb153398f7
bazhenovairinaa <145771526+bazhenovairinaa@users.noreply.github.com>
2024-03-28 19:42:26 +0300

    Add files via upload

---
+++ javascript/java-script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $(".dust1").draggable();
+  $(".dust2").draggable();
+  $(".dust3").draggable();
+  $(".dust4").draggable();
+  $(".dust5").draggable();
+  $(".dust6").draggable();
+  $(".line1").draggable();
+  $(".line2").draggable();
+  $(".line3").draggable();
+  $(".line4").draggable();
+  $(".line5").draggable();
+  $(".line6").draggable();
+  $(".line7").draggable();
+  $(".line8").draggable();
+  $(".line9").draggable();
+  $(".line10").draggable();
+  $(".line11").draggable();
+  $(".line12").draggable();
+  $(".star1").draggable();
+  $(".star2").draggable();
+  $(".star3").draggable();
+  $(".star4").draggable();
+  $(".star5").draggable();
+  $(".star6").draggable();
+  $(".fallingstar1").click(function () {
+    $(this).toggle();
+  });
+  $(".fallingstar2").click(function () {
+    $(this).toggle();
+  });
+  $(".fallingstar11").click(function () {
+    $(this).toggle();
+  });
+  $(".fallingstar12").click(function () {
+    $(this).toggle();
+  });
+  $(".fallingstar13").click(function () {
+    $(this).toggle();
+  });
+  $(".fallingstar14").click(function () {
+    $(this).toggle();
+  });
+  $(".fallingstar15").click(function () {
+    $(this).toggle();
+  });
+});
